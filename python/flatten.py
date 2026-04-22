4ece714b9952840c76f5209deadf577ebba5a371
ramanareddy1995 <narraramanareddy@fgmail.com>
2026-04-22 11:13:23 +0530

    added flatten

---
+++ python/flatten.py
@@ -0,0 +1,11 @@
+def flatten(lst):
+    result = []
+    for item in lst:
+        if isinstance(item,list):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+    return result
+lst = [1, 2, [3, 4], [5,[6, 7]], 8]
+print(flatten(lst))        
+    
