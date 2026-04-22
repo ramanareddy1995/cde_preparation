165436a2e94cf03dd55f91ded1b6a4e6d10d42da
ramanareddy1995 <narraramanareddy@fgmail.com>
2026-04-22 11:17:26 +0530

    removed unused ffiles

--- palindrom.py
+++
@@ -1,8 +0,0 @@
-s="madam"
-result=""
-for i in s:
-    result=i+result
-if result == s:
-    print("palindrom")
-else:
-    print("not palindrome")
--- vowels_cnt.py
+++
@@ -1,7 +0,0 @@
-s="education"
-vowels="aeiouAEIOU"
-count=0
-for i in s:
-    if i in vowels:
-        count += 1
-print(count)
