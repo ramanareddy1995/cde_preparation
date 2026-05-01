d78941a345ccb028a1cbd5bca15a6335cf79ed2c
ramanareddy1995 <narraramanareddy@fgmail.com>
2026-05-01 20:50:20 +0530

    added max subarraysum

---
+++ python/max_subarray_sum.py
@@ -0,0 +1,9 @@
+def max_subarray(nums):
+    nums = [-2,1,-3,4,-1,2,1,-5,4]
+    max_sum = nums[0]
+    current_sum = nums[0]
+    for i in range(1, len(nums)):
+        current_sum = max(nums[i], current_sum + nums[i])
+        max_sum = max(max_sum, current_sum)
+    return max_sum
+print(max_subarray([-2,1,-3,4,-1,2,1,-5,4]))    
